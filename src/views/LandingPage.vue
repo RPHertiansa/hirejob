ab10b970e9e1ab556475f1d5dec125ac42bc4279
alisaid-sm <alisaid7170@gmail.com>
2020-10-10 11:13:26 +0700

    landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>LandingPage</div>
+  <div>
+    <Navbar/>
+    <div>LandingPgae</div>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 export default {
+  components: {
+    Navbar,
+    Footer
+  }
 
 }
 </script>
