af70979752197a7ff8fc2a413e88dc54083c15da
Alfahan <ali.farhan160@gmail.com>
2020-10-12 01:26:02 +0700

    Portfolio

--- src/components/CardUser.vue
+++ src/components/CardUser.vue
@@ -15,6 +15,38 @@
         <p class="text-secondary" style="margin-bottom: 0;"><img src="../assets/img/map-pin (4) 1.png" alt="mappin"> Purwokerto, Jawa Tengah</p>
         <p class="text-secondary">Freelancer</p>
       </div>
+      <div>
+        <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eum nihil aut ut fugit accusantium ratione magnam, ipsam quaerat laudantium voluptas, rerum neque porro? Voluptatibus non illo quisquam rem nesciunt!</p>
+      </div>
+      <div>
+          <button type="button" class="btn btn-search">Hire</button>
+      </div>
+      <div>
+        <h2>Skill</h2>
+      </div>
+      <div>
+          <button class="btn b">Phyton</button>
+          <button class="btn b">Golang</button>
+          <button class="btn b">Kotlin</button>
+          <button class="btn b">PHP</button>
+          <button class="btn b">CSS</button>
+      </div>
+      <div class="text-secondary mt-3 ">
+        <img src="../assets/img/mail (3) 1.png" alt="">
+          Louistommo@gmail.com
+      </div>
+      <div class="text-secondary mt-3">
+        <img src="../assets/img/instagram.png" alt="">
+        @Louist91
+      </div>
+      <div class="text-secondary mt-3">
+        <img src="../assets/img/gitub.png" alt="">
+        @Louistommo
+      </div>
+      <div class="text-secondary mt-3">
+        <img src="../assets/img/gitlab.png" alt="">
+        @Louistommo91
+      </div>
     </div>
   </div>
 </template>
@@ -45,5 +77,30 @@ export default {
 }
 .user-data {
   width: 100%;
+}
+.b {
+    background: rgba(251, 176, 23, 0.6);
+    margin: 5px;
+    border: 1px solid #FBB017;
+    box-sizing: border-box;
+    border-radius: 4px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 20px;
+    color: #FFFFFF;
+    padding: 5px 20px;
+    margin-right: 10px;
+}
+.btn-search {
+    width: 100%;
+    height: 54px;
+    margin-right: 10px;
+    background: #5E50A1;
+    border-radius: 4px;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    color: #FFFFFF;
 }
 </style>
