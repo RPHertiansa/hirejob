6d804b42094fa0f3e130c33e0752e6e6d52ef411
syamsa nur <syamsanur0@gmail.com>
2020-10-15 00:17:42 +0700

    sebelum_presentasi

--- src/components/Riwayat.vue
+++ src/components/Riwayat.vue
@@ -8,23 +8,18 @@
         <div class="main-content">
         <div class="portofolio mt-4" v-if="switchContent === 0">
           <div class="mx-auto text-center" v-for="(item, index) in dataPor" :key="index">
-            <b-card
-              overlay
-              :img-src="`http://localhost:3000/${item.image2}`"
-              img-alt="Image"
-              img-top
-              class="shadow"
-              style="max-width: 250px; height: "
-            >
-            </b-card>
+            <div class="col" @click="openModal(item)">
+              <img :src="`http://localhost:3000/${item.image2}`">
+            </div>
             <small>{{item.namaaplikasi}}</small>
           </div>
         </div>
-        <div class="exprerience mt-4" v-else>
+
+        <div class="exprerience col mt-4" v-else>
              <div class="experience-list row p-3" v-for="(item, index) in dataPeng" :key="index">
                   <div class="company-profile mr-4">
                     <span>
-                      <img src="../assets/img/gambar_default1.jpeg">
+                      <img src="../assets/img/office_default.png" style="height:100px; width:100px">
                     </span>
                   </div>
                   <div class="experiece-bio">
@@ -37,18 +32,64 @@
         </div>
         </div>
     </div>
+
+    <b-modal id="modal-1" hide-header>
+      <b-col>
+        <label><b>{{displayModal.namaaplikasi}}</b></label>
+        <Carousel :navigationEnabled="true" :autoplay="true" :perPage="3">
+          <Slide class="p-2">
+            <div class="col">
+              <img class="img-border" width="120px" height="120px" :src="`http://localhost:3000/${displayModal.image1}`">
+            </div>
+          </Slide>
+          <Slide class="p-2">
+            <div class="col">
+              <img class="img-border" width="120px" height="120px" :src="`http://localhost:3000/${displayModal.image2}`">
+            </div>
+          </Slide>
+          <Slide class="p-2">
+            <div class="col">
+              <img class="img-border" width="120px" height="120px" :src="`http://localhost:3000/${displayModal.image3}`">
+            </div>
+          </Slide>
+        </Carousel>
+      </b-col>
+      <b-col>
+        <b-row class="text-center">
+          <b-col v-if="displayModal.tipeportofolio === 'Aplikasi Web'">
+            <b-icon icon="laptop" font-scale="2" class="mr-3"></b-icon>
+            <p>{{displayModal.tipeportofolio}}</p>
+          </b-col>
+          <b-col v-else>
+            <b-icon icon="phone" font-scale="2" class="mr-3"></b-icon>
+            <p>{{displayModal.tipeportofolio}}</p>
+          </b-col>
+          <b-col>
+            <img src="../assets/img/gitub.png" alt="">
+            <p>{{displayModal.linkrepository}}</p>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-modal>
+
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { Carousel, Slide } from 'vue-carousel'
 
 export default {
+  components: {
+    Carousel,
+    Slide
+  },
   data () {
     return {
       switchToggle: 0,
       switchContent: 0,
-      dataSkill: ['PHP', 'Phyton']
+      dataSkill: ['PHP', 'Phyton'],
+      displayModal: ''
     }
   },
   props: ['idpekerja'],
@@ -81,6 +122,12 @@ export default {
       }
       this.switchContent = 1
     },
+    openModal (item) {
+      // console.log(item)
+      this.displayModal = item
+      // console.log(this.displayModal)
+      this.$bvModal.show('modal-1')
+    },
     ...mapActions({
       getPortofolio: 'pekerja/getPortofolio',
       getPengalaman: 'pekerja/getPengalaman'
@@ -102,18 +149,30 @@ export default {
 }
 </script>
 <style scoped>
+.modalPor img {
+  height: 20vh;
+  width: 20vh;
+}
+
+/* .card-portofolio img{
+  height: 20vh;
+  width: 20vh;
+} */
+
 .portofolio-page {
   background-color: #F6F7F8;
   height: 130vh;
 }
 .portofolio-content {
   background-color: #5E50A1;
-  height: 300px;
+  /* height: 300px; */
 }
 .portofolio-experience {
   background-color: #fff;
   padding: 20px;
   border-radius: 8px;
+  overflow: scroll;
+  height: 100vh;
 }
 .navbar-portofolio {
   display: flex;
@@ -146,6 +205,10 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(219px, 1fr));
   gap: 12px;
+}
+.portofolio img{
+  width: 100px;
+  height: 100px;
 }
 .portofolio-pict {
   width: 219px;
